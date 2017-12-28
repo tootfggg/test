e8c368871f99e1cbfca9565a2da9f40c2546ae34
tootfggg <tootfggg@naver.com>
2017-12-28 10:28:54 +0900

    test project 작성

---
+++ test/src/test/PersonVO.java
@@ -0,0 +1,35 @@
+package test;
+
+public class PersonVO {
+	
+	private String name;
+	private String addr;
+	private String tel;
+	private int no;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	
+}
