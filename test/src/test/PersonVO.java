5e3f57d40fac0a5ac594a7da4a6b528319720d6b
tootfggg <tootfggg@naver.com>
2017-12-28 10:48:37 +0900

    hotfix 성별 추가

--- test/src/test/PersonVO.java
+++ test/src/test/PersonVO.java
@@ -7,7 +7,14 @@ public class PersonVO {
 	private String tel;
 	private int no;
 	private int age;
+	private String sex;
 	
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
 	public String getName() {
 		return name;
 	}
